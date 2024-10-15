14bd25b5e0debed257dfcebaa44c2c70c1b52ffe
Valdery Alves <valdery.alves@dbccompany.com.br>
2024-10-15 15:18:19 -0300

    fix: fix error on build
    Signed-off-by: Valdery Alves <valdery.alves@dbccompany.com.br>

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -11,6 +11,7 @@ import { setScreenSize } from "../store/actions/uiActions";
 // @ts-ignore
 const myIcon = "/static/imgs/close-tag.png";
 import theme from "./app.theme";
+import Script from "next/script";
 
 const mapStateToProps = (store: IReduxStates) => ({
   smoothScroll: store.uiState.smoothScroll,
@@ -50,34 +51,6 @@ const App = ({
     >
       <GlobalStyle smoothScroll={smoothScroll} />
       <Head>
-      {/* Start Meta Pixel Code */}
-      <script>
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '503632712515712');
-      fbq('track', 'PageView');
-      </script>
-      <noscript><img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=503632712515712&ev=PageView&noscript=1"
-      /></noscript>
-      {/* End Meta Pixel Code */}
-
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16729447971"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-16729447971');
-      </script>
-      {/* End Google tag (gtag.js) */}
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
@@ -102,6 +75,25 @@ const App = ({
         <meta name="theme-color" content={theme.green.primary} />
         <title>Valdery - desenvolvedor de sites</title>
       </Head>
+      {/* Start Meta Pixel Code */}
+      <Script id="fb-pixel" src="/fb-pixel.js" />
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=503632712515712&ev=PageView&noscript=1"
+      /></noscript>
+      {/* End Meta Pixel Code */}
+
+      {/* Google tag (gtag.js) */}
+      <Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=AW-16729447971" />
+      <Script id="gtag-init">
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag("js", new Date());
+
+        gtag('config', 'AW-16729447971');
+      </Script>
+      {/* End Google tag (gtag.js) */}
       <HeaderLayout>
         <Component {...pageProps} />
       </HeaderLayout>
